32639a2abb3d9d9907826d8ba5fb8a8543479c15
artur-pinheiro <artur.pinheiro@pequenasempresas.net>
2020-10-29 22:28:37 -0300

    Action Creator to add item to cart

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import api from '../services/api';
 
 import { IProduct } from '../store/modules/cart/types';
+import api from '../services/api';
+import CatalogItem from './CatalogItem';
 
 const Catalog: React.FC = () => {
   const [catalog, setCatalog] = useState<IProduct[]>([])
@@ -17,12 +18,7 @@ const Catalog: React.FC = () => {
       <h1>Catalog</h1>
 
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <span>{product.price}</span> {"  "}
-
-          <button type="button">Comprar</button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   )
